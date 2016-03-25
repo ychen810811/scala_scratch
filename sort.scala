55ba42a69075f49080cfa48df30a435f6f701602
Y. Chen <chen.elliott@gmail.com>
2016-03-25 16:37:07 +0800

    Code experiment the trait and ordering

--- sort.scala
+++ sort.scala
@@ -16,3 +16,34 @@ object Sort {
     }
 
 }
+
+
+trait Ord {
+    def < (that: Any): Boolean
+    def <=(that: Any): Boolean = (this < that) || (this == that)
+    def > (that: Any): Boolean = !(this <= that)
+    def >=(that: Any): Boolean = !(this < that)
+}
+
+class Pair(k: Int, v: String) extends Ord {
+    def key = k
+    def value = v
+    override def toString(): String = key + "=>" + value
+    override def ==(that: Any): Boolean =
+        that.isInstanceOf[Pair] && {
+            val o = that.asInstanceOf[Date]
+            o.key == key
+        }
+    override def <(that: Any): Boolean =
+        that.isInstanceOf[Pari] && {
+            val o = that.asInstanceOf[Date]
+            o.key > key
+        }
+}
+
+object Order {
+    def main(args: Array[String]) {
+        val t = Array[Pair](Pair(3, "hello"), Pair(4, "kick"), Pair(1, "oooops"), Pair(0, "fcuk"))
+        println(t)
+    }
+}
